aae2e019c3fbde12406063971872afa2db8aa30e
Wim Looman <ghostunderscore@gmail.com>
2012-02-04 00:21:04 +1300

    Move direction to it's own class

--- src/camera.hpp
+++ src/camera.hpp
@@ -1,38 +1,7 @@
 #pragma once
 
+#include "direction.hpp"
 #include "geom.hpp"
-
-enum Direction {
-    STILL   =  0,
-    FORWARD =  1,
-    BACK    =  2,
-    RIGHT   =  4,
-    LEFT    =  8,
-    UP      = 16,
-    DOWN    = 32,
-};
-
-inline Direction operator| (Direction lhs, Direction rhs) {
-    return (Direction) ((int)lhs | (int)rhs);
-}
-
-inline Direction& operator|= (Direction& lhs, Direction rhs) {
-    lhs = lhs | rhs;
-    return lhs;
-}
-
-inline Direction operator& (Direction lhs, Direction rhs) {
-    return (Direction) ((int)lhs & (int)rhs);
-}
-
-inline Direction& operator&= (Direction& lhs, Direction rhs) {
-    lhs = lhs & rhs;
-    return lhs;
-}
-
-inline Direction operator~ (Direction& dir) {
-    return (Direction) (~(int)dir);
-}
 
 class Camera {
     public:
